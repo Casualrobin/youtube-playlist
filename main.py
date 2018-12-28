cb5411a01be3433bdc8f1635822fc8f16bf6c4a0
Robin <cannellrobin@hotmail.com>
2018-12-28 12:33:28 +0000

    regex artist

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+url = input("Enter a website to extract the URL's from: ")
+if not url:
+  url = "www.youtube.com/playlist?list=PLvdtkdCcH2D3BWrdv2yMwIJ7-ScsklImS&disable_polymer=true"
 
-#url = input("Enter a website to extract the URL's from: ")
-r  = requests.get("http://" +url)
+r = requests.get("https://" + url)
 
-
-r = requests.get("https://www.youtube.com/playlist?list=PLvdtkdCcH2D3BWrdv2yMwIJ7-ScsklImS&disable_polymer=true")
 data = r.text
 soup = BeautifulSoup(data)
 
@@ -17,8 +17,13 @@ for td in soup.find_all('td'):
     for var in td.find_all('a'):
       if var.get('class')[0] == 'pl-video-title-link':
         print('Song: ' + var.get_text().strip())
-        print('Link: www.youtube.com'+ var.get('href'))
+        print('Link: www.youtube.com' + var.get('href'))
     for div in td.find_all('div'):
       if div.get('class')[0] == 'pl-video-owner':
-        print('Artist / Video Channel: ' + div.get_text().strip())
+        if re.search(r'(?<=by ).+(?= - Topic)', div.get_text().strip()):
+          print('Artist / Video Channel: ' + re.search(r'(?<=by ).+(?= - Topic)', div.get_text().strip()).group())
+        elif re.search(r'(?<=by ).+', div.get_text().strip()):
+          print('Artist / Video Channel: ' + re.search(r'(?<=by ).+', div.get_text().strip()).group())
+        else:
+          print('Regex did not match.')
     print('---------------------------------------')
